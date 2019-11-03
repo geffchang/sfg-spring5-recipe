5c61659406f3ca1024a93d2966e056d879b0e146
geffchang <geffchang@gmail.com>
2019-11-03 22:09:08 +0800

    create spring starter project

---
+++ src/main/java/com/chang/SfgSpring5RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.chang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpring5RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgSpring5RecipeApplication.class, args);
+	}
+
+}
